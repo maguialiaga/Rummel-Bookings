ddd68926a183798deddfeb89e9d517221330837f
maguialiaga <maguialiaga1@gmail.com>
2023-11-15 11:04:34 +0100

    folder

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/magdalenaaliaga/Documents/2023/Rummel-Bookings/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/magdalenaaliaga/Documents/2023/Rummel-Bookings/src/pages/index.js"))
+}
+
